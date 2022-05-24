5fdc4b0af2bff64956afaf79f63b0ecd5e3df727
lowhan <b022010033@student.utem.edu.my>
2022-05-25 01:40:54 +0800

    initial commit

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ const bodyParser = require('body-parser')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.use(bodyParser.json())
+
 ////////////////////////////////////////////////
 //testing
 app.get('/', (req, res) => {
