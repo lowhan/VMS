28c2670738beab85172a188b6db2cc20c1eb97b9
lowhan <b022010033@student.utem.edu.my>
2022-06-16 18:44:06 +0800

    initial commit

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ MongoClient.connect(
 // web application framework for node.js HTTP applications
 const express = require('express')
 const app = express()
-const port = 3000 //const port = process.env.PORT || 3000 
+//const port = 3000 
+const port = process.env.PORT || 3000 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
